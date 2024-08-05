9b7b07dc07962279cf13a2f6b1ef97d59facd7b1
junyu95 <tjy941212@me.com>
2024-08-06 00:10:18 +0800

    test

--- script/main.js
+++ script/main.js
@@ -35,7 +35,7 @@ const animationTimeline = () => {
     .join("</span><span>")}</span`;
 
   hbd.innerHTML = `<span>${hbd.innerHTML
-    .split("")
+    .split("#")
     .join("</span><span>")}</span`;
 
   const ideaTextTrans = {
@@ -292,6 +292,25 @@ const animationTimeline = () => {
       "+=1"
     );
 
+    setTimeout(() => {
+      tl.pause();
+    }, 1200);
+
+    let clicks = 0;
+    // 监听点击事件
+    document.addEventListener('click',function(event) {
+      if (clicks === 0) {
+        var music = document.getElementById("dd");
+        music.play();
+      }
+      if (clicks % 2 === 0) {
+        tl.resume();
+      } else {
+        tl.pause();
+      }
+      clicks++;  
+    });
+
   // tl.seek("currentStep");
   // tl.timeScale(2);
 
@@ -300,9 +319,6 @@ const animationTimeline = () => {
   replyBtn.addEventListener("click", () => {
     tl.restart();
   });
-  // debugger;
-  //   var music = document.getElementById("dd");
-  //   music.play();
 };
 
 // Run fetch and animation in sequence
